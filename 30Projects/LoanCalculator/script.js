6c17ea8c2d5eff8293959dc630aa702b59c26f4f
Ivan Milenkovic <argentinaivan@hotmail.com>
2024-05-29 21:31:26 +0200

    update

---
+++ 30Projects/LoanCalculator/script.js
@@ -0,0 +1,16 @@
+function calculateLoan() {
+  let loanAmountValue = document.getElementById("loan-amount").value;
+  let interestRateValue = document.getElementById("interest-rate").value;
+  let monthsToPayValue = document.getElementById("months-to-pay").value;
+
+  let interest =
+    (loanAmountValue * (interestRateValue * 0.01)) / monthsToPayValue;
+
+  let monthlyPayment = (loanAmountValue / monthsToPayValue + interest).toFixed(
+    2
+  );
+
+  document.getElementById(
+    "payment"
+  ).innerHTML = `Monthly Payment: ${monthlyPayment}`;
+}
