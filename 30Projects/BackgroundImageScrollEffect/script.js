66d99db71c8048c7cd6762dafd7aba6605ef1f04
Ivan Milenkovic <argentinaivan@hotmail.com>
2024-07-22 01:36:50 +0200

    update

---
+++ 30Projects/BackgroundImageScrollEffect/script.js
@@ -0,0 +1,10 @@
+const bgImageEl = document.getElementById("bg-image");
+
+function updateImage() {
+  bgImageEl.style.opacity = 1 - window.pageYOffset / 900;
+  bgImageEl.style.backgroundSize = 160 - window.pageYOffset / 12 + "%";
+}
+
+window.addEventListener("scroll", () => {
+  updateImage();
+});
