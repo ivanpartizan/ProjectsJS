9ce88a08ac65670747a73568581058f9a2d117ba
Ivan Milenkovic <argentinaivan@hotmail.com>
2024-09-20 14:47:14 +0200

    update

--- 10Projects/WeatherApp/script.js
+++ 10Projects/WeatherApp/script.js
@@ -22,14 +22,33 @@ async function getWeatherData(cityValue) {
     }
 
     const data = await response.json();
+    console.log(data);
 
     const temperature = Math.round(data.main.temp);
     const description = data.weather[0].description;
     const icon = data.weather[0].icon;
     const details = [
       `Feels like: ${Math.round(data.main.feels_like)}`,
-      `Humidity: ${data.main.humidity}`,
-      `Wind speed: ${data.wind.speed}`,
+      `Humidity: ${data.main.humidity}%`,
+      `Wind speed: ${data.wind.speed} m/s`,
     ];
-  } catch (error) {}
+
+    weatherDataEl.querySelector(".icon").innerHTML = ` <img
+            src="http://openweathermap.org/img/wn/${icon}.png"
+            alt="Weather Icon"
+          />`;
+    weatherDataEl.querySelector(".temperature").textContent = `${temperature}°C
+          `;
+    weatherDataEl.querySelector(".description").textContent = description;
+
+    weatherDataEl.querySelector(".details").innerHTML = details
+      .map((detail) => `<div>${detail}</div>`)
+      .join("");
+  } catch (error) {
+    weatherDataEl.querySelector(".icon").innerHTML = "";
+    weatherDataEl.querySelector(".temperature").textContent = "";
+    weatherDataEl.querySelector(".description").textContent =
+      "An error happened, please try again later";
+    weatherDataEl.querySelector(".details").innerHTML = "";
+  }
 }
