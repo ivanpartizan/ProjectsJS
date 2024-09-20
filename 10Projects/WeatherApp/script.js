4bc7f52dd9f7a947575c276aae903e8ad40a248d
Ivan Milenkovic <argentinaivan@hotmail.com>
2024-09-20 03:09:00 +0200

    update

--- 10Projects/WeatherApp/script.js
+++ 10Projects/WeatherApp/script.js
@@ -23,6 +23,13 @@ async function getWeatherData(cityValue) {
 
     const data = await response.json();
 
-    console.log(data);
+    const temperature = Math.round(data.main.temp);
+    const description = data.weather[0].description;
+    const icon = data.weather[0].icon;
+    const details = [
+      `Feels like: ${Math.round(data.main.feels_like)}`,
+      `Humidity: ${data.main.humidity}`,
+      `Wind speed: ${data.wind.speed}`,
+    ];
   } catch (error) {}
 }
