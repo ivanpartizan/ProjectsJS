93416f73c66ee87dc6704e9e810055768c2429de
Ivan Milenkovic <argentinaivan@hotmail.com>
2024-09-20 02:47:24 +0200

    update

---
+++ 10Projects/WeatherApp/script.js
@@ -0,0 +1,28 @@
+const apiKey = "7b31fc51d965595f7ad494ff430bf91f";
+
+const weatherDataEl = document.getElementById("weather-data");
+const cityInputEl = document.getElementById("city-input");
+const formEl = document.querySelector("form");
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const cityValue = cityInputEl.value;
+
+  getWeatherData(cityValue);
+});
+
+async function getWeatherData(cityValue) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`
+    );
+
+    if (!response.ok) {
+      throw new Error("Network response was not OK");
+    }
+
+    const data = await response.json();
+
+    console.log(data);
+  } catch (error) {}
+}
