3cea72c236946f844e0c7e3e4aa90f16faad5783
Ivan Milenkovic <argentinaivan@hotmail.com>
2024-09-27 03:06:28 +0200

    update

---
+++ 10Projects/ImageSearchApp/script.js
@@ -0,0 +1,6 @@
+const accessKey = "-JLuYTo7apgvuHq7T07PLPertrKqgkmWQNGfHibX7zQ";
+
+const formEl = document.querySelector("form");
+const searchInputEl = document.getElementById("search-input");
+const searchResultsEl = document.querySelector(".search-results");
+const showMoreButtonEl = document.getElementById("show-more-button");
