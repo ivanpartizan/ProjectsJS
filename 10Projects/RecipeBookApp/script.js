703f884b7b44cc8db08f63c0de56c5e2e902dece
Ivan Milenkovic <argentinaivan@hotmail.com>
2024-11-12 02:32:44 +0100

    update

--- 10Projects/RecipeBookApp/script.js
+++ 10Projects/RecipeBookApp/script.js
@@ -1,3 +1,47 @@
-function init() {}
+const API_KEY = "10ce5f97d9e845c3bc827a72d7583b3e";
+const recipeListEl = document.getElementById("recipe-list");
+
+function displayRecipes(recipes) {
+  recipeListEl.innerHTML = "";
+  recipes.forEach((recipe) => {
+    const recipeItemEl = document.createElement("li");
+    recipeItemEl.classList.add("recipe-item");
+    const recipeImageEl = document.createElement("img");
+    recipeImageEl.src = recipe.image;
+    recipeImageEl.alt = "recipe image";
+    const recipeTitleEl = document.createElement("h2");
+    recipeTitleEl.innerText = recipe.title;
+    const recipeIngredientsEl = document.createElement("p");
+    recipeIngredientsEl.innerHTML = `<strong>Imgredients: </strong>${recipe.extendedIngredients
+      .map((ingredient) => ingredient.original)
+      .join(", ")}`;
+    const recipeLinkEl = document.createElement("a");
+    recipeLinkEl.href = recipe.sourceUrl;
+    recipeLinkEl.innerText = "View Recipe";
+
+    recipeItemEl.appendChild(recipeImageEl);
+    recipeItemEl.appendChild(recipeTitleEl);
+    recipeItemEl.appendChild(recipeIngredientsEl);
+    recipeItemEl.appendChild(recipeLinkEl);
+
+    recipeListEl.appendChild(recipeItemEl);
+  });
+}
+
+async function getRecipes() {
+  const response = await fetch(
+    `https://api.spoonacular.com/recipes/random?number=10&apiKey=${API_KEY}`
+  );
+
+  const data = await response.json();
+
+  return data.recipes;
+}
+
+async function init() {
+  const recipes = await getRecipes();
+
+  displayRecipes(recipes);
+}
 
 init();
