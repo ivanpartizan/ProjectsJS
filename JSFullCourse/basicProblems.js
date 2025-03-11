af7319a470166249b23c8a5cc30b3406a5e59181
Ivan Milenkovic <argentinaivan@hotmail.com>
2025-03-11 21:47:57 +0100

    update

---
+++ JSFullCourse/basicProblems.js
@@ -0,0 +1,21 @@
+// 8kyu
+// Multiply
+function multiply(a, b) {
+  return a * b;
+}
+
+multiply(5, 2);
+
+// Even or Odd
+function evenOrOdd(number) {
+  return number % 2 === 0 ? "Even" : "Odd";
+}
+
+evenOrOdd(2);
+
+// Reversed Strings
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+reverseString("world");
