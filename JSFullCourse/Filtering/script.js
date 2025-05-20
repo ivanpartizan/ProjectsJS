eff107df1cf5ef2fb5c3917ab606aa5f2fabb2c1
Ivan Milenkovic <argentinaivan@hotmail.com>
2025-05-20 23:47:35 +0200

    update

--- JSFullCourse/Filtering/script.js
+++ JSFullCourse/Filtering/script.js
@@ -140,3 +140,25 @@ function updateCard(e) {
 
   cartCount.innerText = cartItemCount.toString();
 }
+
+function filterProducts() {
+  const searchTerm = searchInput.value.trim().toLowerCase();
+  const checkedCategories = Array.from(checkboxes)
+    .filter((check) => check.checked)
+    .map((check) => check.id);
+
+  productElements.forEach((productElement, index) => {
+    const product = products[index];
+
+    const matchesSearchTerm = product.name.toLowerCase().includes(searchTerm);
+    const isInCheckedCategory =
+      checkedCategories.length === 0 ||
+      checkedCategories.includes(product.category);
+
+    if (matchesSearchTerm && isInCheckedCategory) {
+      productElement.classList.remove("hidden");
+    } else {
+      productElement.classList.add("hidden");
+    }
+  });
+}
