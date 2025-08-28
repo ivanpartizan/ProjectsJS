ed11cbc4e56e1d81684f32d0798643c00691e1b1
Ivan Milenkovic <argentinaivan@hotmail.com>
2025-08-28 23:13:59 +0200

    update

---
+++ 15Projects/Counter/app.js
@@ -0,0 +1,15 @@
+let count = 0;
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    }
+
+    value.textContent = count;
+  });
+});
