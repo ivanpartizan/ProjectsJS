591cf228262def2d8d2c2f4134d1bbfe55111b94
Ivan Milenkovic <argentinaivan@hotmail.com>
2025-09-15 01:42:41 +0200

    update

--- 15Projects/Reviews/app.js
+++ 15Projects/Reviews/app.js
@@ -42,11 +42,11 @@ const randomBtn = document.querySelector(".random-btn");
 let currentItem = 0;
 
 window.addEventListener("DOMContentLoaded", function () {
-  showPerson(currentItem);
+  showPerson();
 });
 
-function showPerson(person) {
-  const item = reviews[person];
+function showPerson() {
+  const item = reviews[currentItem];
   img.src = item.img;
   author.textContent = item.name;
   job.textContent = item.job;
@@ -55,10 +55,21 @@ function showPerson(person) {
 
 nextBtn.addEventListener("click", function () {
   currentItem++;
-  showPerson(currentItem);
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson();
 });
 
 prevBtn.addEventListener("click", function () {
   currentItem--;
-  showPerson(currentItem);
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson();
+});
+
+randomBtn.addEventListener("click", function () {
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson();
 });
