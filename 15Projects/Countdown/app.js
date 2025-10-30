95442907657d67b5279764e2613b0b345bd0c34c
Ivan Milenkovic <argentinaivan@hotmail.com>
2025-10-30 23:08:11 +0100

    update

--- 15Projects/Countdown/app.js
+++ 15Projects/Countdown/app.js
@@ -29,5 +29,12 @@ const items = document.querySelectorAll(".deadline-format h4");
 let futureDate = new Date(2026, 9, 12, 11, 30, 0);
 
 const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
 
-giveaway.textContent = `giveaway ends on ${year}`;
+let month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDate();
+const weekday = weekdays[futureDate.getDay()];
+
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
