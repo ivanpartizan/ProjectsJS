a8f2df4268f915df4d8900197e89ec0936dace7f
Ivan Milenkovic <argentinaivan@hotmail.com>
2025-11-09 23:51:32 +0100

    update

--- 15Projects/Countdown/app.js
+++ 15Projects/Countdown/app.js
@@ -26,7 +26,13 @@ const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
-let futureDate = new Date(2026, 9, 12, 11, 30, 0);
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
+
+// let futureDate = new Date(2026, 9, 12, 11, 30, 0);
+let futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
@@ -67,6 +73,13 @@ function getRemainingTime() {
   items.forEach(function (item, index) {
     item.innerHTML = format(values[index]);
   });
+
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class='expired'>sorry, this giveaway has expired</h4>`;
+  }
 }
+
+let countdown = setInterval(getRemainingTime, 1000);
 
 getRemainingTime();
