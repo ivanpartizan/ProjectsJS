89b872ef1eaf14f1c246ac99ca9029df5fbb74b3
Ivan Milenkovic <argentinaivan@hotmail.com>
2025-12-07 02:21:28 +0100

    update

---
+++ 15Projects/GroceryBud/app.js
@@ -0,0 +1,19 @@
+const alert = document.querySelector(".alert");
+const form = document.querySelector(".grocery-form");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
+
+let editElement;
+let editFlag = false;
+let editID = "";
+
+form.addEventListener("submit", addItem);
+
+function addItem(e) {
+  e.preventDefault();
+  const value = grocery.value;
+  const id = new Date().getTime().toString();
+}
